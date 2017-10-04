e7f176f048f88199ca22ddd58fa8de2a79aa9434
Thi hoai thu NINH <thninh@e1r3p2.42.fr>
2017-10-04 18:07:33 +0200

    ex08

---
+++ d01/ex08/ft_is_sort.php
@@ -0,0 +1,12 @@
+<?php
+	function ft_is_sort($tab)
+	{
+		$tab_sort = $tab;
+		sort($tab_sort);
+		print_r($tab_sort);
+		if (array_diff_assoc($tab, $tab_sort ) == NULL)
+			return true;
+		else
+			return false;
+	}
+?>
