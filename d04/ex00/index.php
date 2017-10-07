9fa55b8e4fafaab25f43d5a2c7e4ebf77a2ee752
Thi hoai thu NINH <thninh@e1r6p1.42.fr>
2017-10-07 14:15:06 +0200

    ex00

---
+++ d04/ex00/index.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+
+	if ($_GET['login'] && $_GET['passwd'] && $_GET['submit'] && $_GET['submit'] === "OK")
+	{
+		$_SESSION['login'] = $_GET['login'];
+		$_SESSION['passwd'] = $_GET['passwd'];
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+	<body>
+		<form action="index.php" method="GET">
+			Identifiant:   <input type="text" name="login" value="<?php echo $_SESSION['login'];?>" /><br/>
+			Mot de pass: <input type="password" name="passwd" value="<?php echo $_SESSION['passwd'];?>" /><br/>
+			<input type="submit" name="submit" value="OK">
+		</form>
+	</body>
+</html>
