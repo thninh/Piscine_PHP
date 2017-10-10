a6379886a773f2317c61c3d194a1c268191e65ff
Thi Hoai Thu NINH <thninh@student.42.fr>
2017-10-10 13:08:02 +0200

    Update ex01.sql

--- d05/ex01/ex01.sql
+++ d05/ex01/ex01.sql
@@ -1,6 +1,6 @@
 CREATE TABLE ft_table(
   id INT PRIMARY KEY NOT NULL,
-  login VARCHAR() NOT NULL DEFAULT "toto",
+  login VARCHAR(10) NOT NULL DEFAULT "toto",
   groupe ENUM('staff', 'student', 'other') NOT NULL,
   date_de_creation DATE NOT NULL
 );
