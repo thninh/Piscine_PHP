87c5775ffa3ac0d9f1573266276b947a0f3cc8e5
Thi Hoai Thu NINH <ninhhoaithu@MacBook-Pro-de-Thi.local>
2017-10-10 12:55:35 +0200

    ex20

---
+++ d05/ex20/ex20.sql
@@ -0,0 +1,8 @@
+SELECT genre.id_genre AS 'id_genre',
+        genre.nom AS 'nom genre',
+        distrib.id_distrib AS 'id_distrib',
+        distrib.nom AS 'nom distrib',
+        film.titre AS 'titre film' FROM film
+LEFT JOIN genre ON film.id_genre = genre.id_genre
+LEFT JOIN distrib ON film.id_distrib = distrib.id_distrib
+WHERE film.id_genre BETWEEN 4 AND 8;
