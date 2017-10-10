62df116033992c8c179f6c7384589069becdb45a
Thi Hoai Thu NINH <ninhhoaithu@MacBook-Pro-de-Thi.local>
2017-10-10 09:37:34 +0200

    ex01

--- d05/ex01/ex01.sql
+++ d05/ex01/ex01.sql
@@ -1,6 +1,6 @@
 CREATE TABLE ft_table(
   id INT PRIMARY KEY NOT NULL,
   login VARCHAR() NOT NULL DEFAULT "toto",
-  group ENUM('staff', 'student', 'other') NOT NULL,
+  groupe ENUM('staff', 'student', 'other') NOT NULL,
   date_de_creation DATE NOT NULL
 );
